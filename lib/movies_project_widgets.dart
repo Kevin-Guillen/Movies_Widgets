c430a86fc3e2e2b6d48a67c9eb99e5b129c04696
kevin-guillen <kevin.guillen@globant.com>
2021-07-19 12:33:54 -0300

    movies project widgets migration

---
+++ lib/movies_project_widgets.dart
@@ -0,0 +1,6 @@
+library movies_widgets;
+
+export 'src/widgets/custom_appbar.dart';
+export 'src/widgets/movie_description.dart';
+export 'src/widgets/movie_details_page.dart';
+export 'src/widgets/movie_main_info.dart';
